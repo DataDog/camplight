f14f4e6ded40c48647a33f874aa85aa995b7f7d9
whatarthurcodes <arthur.yan@datadoghq.com>
2014-10-17 19:26:27 +0000

    Change Version number to post format

--- camplight/__init__.py
+++ camplight/__init__.py
@@ -10,7 +10,7 @@ Python implementation of Campfire API
 
 __author__ = 'Mathias Lafeldt <mathias.lafeldt@gmail.com>'
 __license__ = 'MIT'
-__version__ = '0.9.6-1'
+__version__ = '0.9.6.post1'
 
 from .api import *
 from .exceptions import *
